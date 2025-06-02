c204f02517274399d2d4201620c085e71007ff77
Naila Nurzalika <naila23si@mahasiswa.pcr.ac.id>
2025-06-02 15:57:54 +0700

    revisi app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,23 @@ import Dashboard from './pages/Dashboard'
 import MainLayout from './components/MainLayout'
 import SalesManagement from './pages/SalesManagement'
 import Customers from './pages/Customers'
-import Product from './pages/Product';
-import Product from './pages/Products'
+import Product from './pages/Product'
 import Login from './pages/Login'
 import Register from './pages/Register'
 
 function App() {
   return (
     <Routes>
+      {/* Routes tanpa layout */}
+      <Route path="/signin" element={<Login />} />
+      <Route path="/signup" element={<Register />} />
+
+      {/* Routes dengan layout */}
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/penjualan" element={<SalesManagement/>} />
+        <Route path="/penjualan" element={<SalesManagement />} />
         <Route path="/pelanggan" element={<Customers />} />
         <Route path="/produk" element={<Product />} />
-        <Route path="/produk" element={<Product/>} />
-        <Route path="/signin" element={<Login/>} />
-        <Route path="/signup" element={<Register/>} />
       </Route>
     </Routes>
   )
