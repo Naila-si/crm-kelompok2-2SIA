56143b6bb2ed57d95887f5adb008825fc959f6b4
dimas <rdimas23si@mahasiswa.pcr.ac.id>
2025-06-02 14:46:29 +0700

    dimas-product

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import MainLayout from './components/MainLayout'
 import SalesManagement from './pages/SalesManagement'
+import Product from './pages/Products'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
         <Route path="/penjualan" element={<SalesManagement/>} />
+        <Route path="/produk" element={<Product/>} />
       </Route>
     </Routes>
   )
