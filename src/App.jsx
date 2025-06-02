8e559ee623fa18b97b2a2d948d483bbe08c598eb
dimas <rdimas23si@mahasiswa.pcr.ac.id>
2025-06-02 15:46:53 +0700

    dimas-login,regis

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Dashboard from './pages/Dashboard'
 import MainLayout from './components/MainLayout'
 import SalesManagement from './pages/SalesManagement'
 import Product from './pages/Products'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/penjualan" element={<SalesManagement/>} />
         <Route path="/produk" element={<Product/>} />
+        <Route path="/signin" element={<Login/>} />
+        <Route path="/signup" element={<Register/>} />
       </Route>
     </Routes>
   )
