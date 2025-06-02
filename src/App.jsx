b72d4ff33880ec20e481ba16f97e60a17d7c3999
Naila Nurzalika <naila23si@mahasiswa.pcr.ac.id>
2025-06-02 14:52:28 +0700

    revisi tampilan produk

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import MainLayout from './components/MainLayout'
 import SalesManagement from './pages/SalesManagement'
-import Customers from './pages/Customers'; // atau './pages/Pelanggan'
+import Customers from './pages/Customers'; 
 import Product from './pages/Product';
-import Product from './pages/Products'
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <Route path="/penjualan" element={<SalesManagement/>} />
         <Route path="/pelanggan" element={<Customers />} />
         <Route path="/produk" element={<Product />} />
-        <Route path="/produk" element={<Product/>} />
       </Route>
     </Routes>
   )
